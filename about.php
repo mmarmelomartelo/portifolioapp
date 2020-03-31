cf0c6b17ed3d70107b1a837a212c21c2944a6df9
Marcelo Montenegro <montenegromf@gmailcom>
2020-03-31 12:54:59 +0200

    index php commit to horoku

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>  
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="css/style.css" />
+
+<title>Montenegro MF</title>
+  </head>
+<style>
+div {
+  border: 1px solid black;
+  margin: 25px 50px 75px 100px;
+  background-color: lightblue;
+}
+</style>
+
+
+
+  <body>
+    <main>
+      <nav>
+        <h1>Marcelo' Portfolio</h1>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+        </ul>
+      </nav>
+      
+      <div>
+            <nav>
+                <h3>Marcelo Montenegro</h3>
+            </nav>
+            <br/>
+            <p>This is my portifolio. If you like it, just contact me<br/>
+            please see contact details is my online Resume.<br/>
+         As we say in Swedish "Vi ses".</p>
+
+
+      </div>
+    </main>
+   </head>
+  
+
+
+  <br/>
+  <br/>
+  <br/>
+
+ <center>
+      <footer>
+            <p> Copyright Marcelo Montenegro 2020 ©
+      </footer>
+ </center> <br/>
+
+
+ </body>
+</html>
